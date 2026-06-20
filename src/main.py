4966e08b482b211429b5d8ec3f4bb0dd3751ac96
Naplon <94738563+naplon74@users.noreply.github.com>
2026-06-20 16:17:24 +0200

    e

---
+++ src/main.py
@@ -0,0 +1,94 @@
+# MIT License
+#
+# Copyright (c) 2026 Naplon
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# SPDX-License-Identifier: MIT
+
+import sys
+import gi
+
+from gettext import gettext as _
+
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+
+from gi.repository import Gtk, Gio, Adw
+from .window import KeyboardCleanerWindow
+
+
+class KeyboardCleanerApplication(Adw.Application):
+    """The main application singleton class."""
+
+    def __init__(self):
+        super().__init__(application_id='naplon_.keyboard.cleaner',
+                         flags=Gio.ApplicationFlags.DEFAULT_FLAGS,
+                         resource_base_path='/naplon_/keyboard/cleaner')
+        self.create_action('quit', lambda *_: self.quit(), ['<control>q'])
+        self.create_action('about', self.on_about_action)
+        self.create_action('preferences', self.on_preferences_action)
+
+    def do_activate(self):
+        """Called when the application is activated.
+
+        We raise the application's main window, creating it if
+        necessary.
+        """
+        win = self.props.active_window
+        if not win:
+            win = KeyboardCleanerWindow(application=self)
+        win.present()
+
+    def on_about_action(self, *args):
+        """Callback for the app.about action."""
+        about = Adw.AboutDialog(application_name='Keyboard_Cleaner',
+                                application_icon='naplon_.keyboard.cleaner',
+                                developer_name='Naplon_',
+                                version='0.1.0',
+                                # Translators: Replace "translator-credits" with your name/username, and optionally an email or URL.
+                                translator_credits = _('translator-credits'),
+                                developers=['Naplon_'],
+                                copyright='© 2026 Naplon')
+        about.present(self.props.active_window)
+
+    def on_preferences_action(self, widget, _):
+        """Callback for the app.preferences action."""
+        print('app.preferences action activated')
+
+    def create_action(self, name, callback, shortcuts=None):
+        """Add an application action.
+
+        Args:
+            name: the name of the action
+            callback: the function to be called when the action is
+              activated
+            shortcuts: an optional list of accelerators
+        """
+        action = Gio.SimpleAction.new(name, None)
+        action.connect("activate", callback)
+        self.add_action(action)
+        if shortcuts:
+            self.set_accels_for_action(f"app.{name}", shortcuts)
+
+
+def main(version):
+    """The application's entry point."""
+    app = KeyboardCleanerApplication()
+    return app.run(sys.argv)
